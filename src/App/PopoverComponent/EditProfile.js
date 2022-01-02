a3337a5ab6ef918e19f1ea1a7832d364c4e53199
svmx-nitesh <nitesh.kumar@servicemax.com>
2022-01-02 20:36:12 +0530

    skeleton for edit profile

--- src/App/PopoverComponent/EditProfile.js
+++ src/App/PopoverComponent/EditProfile.js
@@ -6,8 +6,27 @@ class EditProfile extends React.Component {
     render() {
         return (
             <div className="layover">
-                EditProfile is active now,
-                <button onClick={() => this.props.changePopoverState("CLOSE_EDIT_PROFILE")}>close_modal</button>
+                <div className="container-fluid h-100">
+                    <div className="row justify-content-center align-content-center h-100">
+                        <div className="col-md-3 border">
+                            <div class="form-outline">
+                                <input type="text" id="typeText" class="form-control" />
+                                <label class="form-label" for="typeText">Text input</label>
+                            </div>
+                            <div class="form-outline">
+                                <input type="email" id="typeEmail" class="form-control" />
+                                <label class="form-label" for="typeEmail">Email input</label>
+                            </div>
+                        </div>
+                        <div className="col-md-3 border">
+                            <div class="form-outline">
+                                <input type="number" id="typeNumber" class="form-control" />
+                                <label class="form-label" for="typeNumber">Phone Number</label>
+                            </div>
+                        </div>  
+                    </div>
+                </div>
+                <button className="btn btn-secondary d-abs-tl" onClick={() => this.props.changePopoverState("CLOSE_EDIT_PROFILE")}>close_modal</button>
             </div>
         )
     }
